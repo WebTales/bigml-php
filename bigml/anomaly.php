9d8b5141b2bc55247b83c44f72067d7bb7194d27
Didier Fanchon <didier.fanchon@webtales.fr>
2016-01-07 14:52:30 +0100

    Update anomaly.php

--- bigml/anomaly.php
+++ bigml/anomaly.php
@@ -14,6 +14,8 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+namespace BigML;
+
 if (!class_exists('modelfields')) {
    include('modelfields.php'); 
 }
